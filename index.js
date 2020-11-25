5f1cc0ffbc70c8618304da28c40c289054deac0f
LuisM <70212840+zLuisOne@users.noreply.github.com>
2020-11-25 08:11:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ client.on("ready" , () =>{
     console.log('╚[═════════════════════════════════════════════════════════════════]╝')
     console.log('')
     console.log('╔[════════════]╗')
-    console.log('Created By: LuisM#1423')
+    console.log('Created By: False#9680')
     console.log('╚[════════════]╝')
-    const actividad = [``,`Bot Easy`,`Prefix: !`,"Owner: LuisM#1423"
+    const actividad = [``,`Bot Easy`,`Prefix: !`,"Owner: False#9680"
       ];
     setInterval(()=>{const index = Math.floor(Math.random() * (actividad.length - 1) + 1 );
     client.user.setPresence({
@@ -43,6 +43,62 @@ client.on("ready" , () =>{
   });
   client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
+
+let commandFiles = readdirSync(join(__dirname, "commands", )).filter((file) => file.endsWith(".js")) 
+
+ for (const file of commandFiles) {
+   const command = require(join(__dirname, "commands" ,  `${file}`));
+   client.commands.set(command.name, command);
+ }
+ 
+ client.on("message", async (message) => {
+   if (message.author.bot) return;
+   if (!message.guild) return;
+ 
+   const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
+   if (!prefixRegex.test(message.content)) return;
+ 
+   const [, matchedPrefix] = message.content.match(prefixRegex);
+ 
+   const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+   const commandName = args.shift().toLowerCase();
+  
+   const command =
+     client.commands.get(commandName) ||
+     client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+ 
+   if (!command) return;
+ 
+   if (!cooldowns.has(command.name)) {
+     cooldowns.set(command.name, new Collection());
+   }
+ 
+   const now = Date.now();
+   const timestamps = cooldowns.get(command.name);
+   const cooldownAmount = (command.cooldown || 1) * 2000;
+ 
+   if (timestamps.has(message.author.id)) {
+     const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+ 
+     if (now < expirationTime) {
+       const timeLeft = (expirationTime - now) / 1000;
+       return message.reply(
+         `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
+       );
+     }
+   }
+ 
+   timestamps.set(message.author.id, now);
+   setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+ 
+   try {
+     command.execute(message, args);
+   } catch (error) {
+     console.error(error);
+     message.reply("There was an error executing that command.").catch(console.error);
+   }
+ });
+
 
 /*
   ____  _             _   
