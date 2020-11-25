15d955ec83db8ee82a890359d0ac49bdf0ddb16b
LuisM <70212840+zLuisOne@users.noreply.github.com>
2020-11-25 08:00:59 -0500

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -1,4 +1,4 @@
-const { canModifyQueue } = require("../util/ComeQueso");
+const { canModifyQueue } = require("../util/Quesito");
 const { MessageEmbed } = require('discord.js');
 
 
@@ -8,7 +8,7 @@ module.exports = {
   description: "Reanudar la reproducción de música actualmente",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("No hay nada reproduciendo.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (!queue.playing) {
@@ -16,7 +16,7 @@ module.exports = {
       queue.connection.dispatcher.resume();
 
       const embed4 = new MessageEmbed()
-            .setTitle(`${user.username} ▶ reanudando transmisión...!`)
+            .setDescription(`**${user.username}** ▶ reanudando transmisión...!`)
             .setColor("#F5ECEC")
       return queue.textChannel.send(embed4).catch(console.error);
     }
